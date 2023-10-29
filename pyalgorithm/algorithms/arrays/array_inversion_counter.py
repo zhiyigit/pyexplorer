35e44d4cd4fd1ff680d99210b8fc37f29ceff065
zhiyi zhang <zhiyi.zhang@hotmail.com>
2023-10-29 11:49:10 -0700

    refine the merge

--- pyalgorithm/algorithms/arrays/array_inversion_counter.py
+++ pyalgorithm/algorithms/arrays/array_inversion_counter.py
@@ -1,8 +1,9 @@
 from array import array
 
 
-def _count_inversion_and_sort_in_array(a: array, start_index: int, end_index: int) -> int:
-    print(f"[loop begin] start {start_index} end {end_index} array: {a}")
+def _count_inversion_and_sort_in_array(
+    a: array, start_index: int, end_index: int
+) -> int:
     if start_index == end_index:
         return 0
 
@@ -16,40 +17,36 @@ def _count_inversion_and_sort_in_array(a: array, start_index: int, end_index: in
             return 0
 
     middle_index = (start_index + end_index) // 2
-    print(f"  middle {middle_index} of start {start_index} and end {end_index}")
     left_end_index = middle_index
     right_start_index = middle_index + 1
-    left_inversion_count = _count_inversion_and_sort_in_array(a, start_index, left_end_index)
-    right_inversion_count = _count_inversion_and_sort_in_array(a, right_start_index, end_index)
+    left_inversion_count = _count_inversion_and_sort_in_array(
+        a, start_index, left_end_index
+    )
+    right_inversion_count = _count_inversion_and_sort_in_array(
+        a, right_start_index, end_index
+    )
 
     additional_inversion_count = 0
     left_index = start_index
     right_index = right_start_index
-    new_array = array('i', [0] * ((end_index - start_index) + 1))
+    left_array_copy = a[start_index:right_start_index]
     while left_index <= left_end_index and right_index <= end_index:
-        new_array_index = (left_index - start_index) + (right_index - right_start_index)
-        if a[left_index] <= a[right_index]:
-            new_array[new_array_index] = a[left_index]
+        update_index = left_index + (right_index - right_start_index)
+        if left_array_copy[left_index - start_index] <= a[right_index]:
+            a[update_index] = left_array_copy[left_index - start_index]
             left_index = left_index + 1
         else:
-            additional_inversion_count = additional_inversion_count + (left_end_index - left_index) + 1
-            new_array[new_array_index] = a[right_index]
+            additional_inversion_count = (
+                additional_inversion_count + (left_end_index - left_index) + 1
+            )
+            a[update_index] = a[right_index]
             right_index = right_index + 1
 
     if left_index <= left_end_index:
         for i in range(left_index, left_end_index + 1):
-            new_array[(i - start_index) + (end_index - left_end_index)] = a[i]
-    elif right_index <= end_index:
-        for i in range(right_index, end_index + 1):
-            new_array[(i - right_start_index) + (right_start_index - start_index)] = a[i]
-
-    print(f"start {start_index} end {end_index} new array: {new_array}")
-
-    for i in range (0, (end_index - start_index) + 1):
-        a[i + start_index] = new_array[i]
+            a[i + (end_index - left_end_index)] = left_array_copy[i - start_index]
 
     count = left_inversion_count + right_inversion_count + additional_inversion_count
-    print(f"[loop end] count {count} start {start_index} end {end_index} array: {a}")
     return count
 
 
--- pyalgorithm/tests/test_array_inversion_counter.py
+++ pyalgorithm/tests/test_array_inversion_counter.py
@@ -4,25 +4,25 @@ from algorithms.arrays.array_inversion_counter import count_inverses_in_array
 
 
 def test_count_inversions_zero():
-    sorted_array = array('i', [1, 2, 3, 4, 5, 6, 7, 8])
+    sorted_array = array("i", [1, 2, 3, 4, 5, 6, 7, 8])
     assert count_inverses_in_array(sorted_array) == 0
 
 
 def test_count_inversions_max_size_3():
-    full_inversion = array('i', [3, 2, 1])
+    full_inversion = array("i", [3, 2, 1])
     assert count_inverses_in_array(full_inversion) == 3
 
 
 def test_count_inversions_max_size_4():
-    full_inversion = array('i', [4, 3, 2, 1])
+    full_inversion = array("i", [4, 3, 2, 1])
     assert count_inverses_in_array(full_inversion) == 6
 
 
 def test_count_inversion_mixed_even_length():
-    inversion_mixed = array('i', [1, 6, 2, 4, 5, 3])
+    inversion_mixed = array("i", [1, 6, 2, 4, 5, 3])
     assert count_inverses_in_array(inversion_mixed) == 6
 
 
 def test_count_inversion_mixed_odd_length():
-    inversion_mixed = array('i', [1, 7, 6, 2, 9, 4, 8,  5, 3])
+    inversion_mixed = array("i", [1, 7, 6, 2, 9, 4, 8, 5, 3])
     assert count_inverses_in_array(inversion_mixed) == 17
